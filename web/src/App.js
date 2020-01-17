be71b59a43aab95f58c8f57004e332651d36145b
Nixoff <gabadev48@protonmail.com>
2020-01-17 02:31:59 -0300

    Interface web 90% finalizada, faltando abstrair componentes

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
 import "./Main.css";
 
+import DevItem from "./components/DevItem";
+
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  const [github_username, setGithubUsername] = useState("");
+  const [techs, setTechs] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       position => {
-        console.log(position);
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
       },
       err => {
         console.log(err);
@@ -19,30 +32,83 @@ function App() {
       }
     );
   }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post("/devs", {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGithubUsername("");
+    setTechs("");
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input
+                type="numer"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input
+                type="numer"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
@@ -52,74 +118,9 @@ function App() {
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
-                alt="Diego Fernandes"
-              />
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
-              desenvolvimento web e mobile.
-            </p>
-            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
-                alt="Diego Fernandes"
-              />
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
-              desenvolvimento web e mobile.
-            </p>
-            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
-                alt="Diego Fernandes"
-              />
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
-              desenvolvimento web e mobile.
-            </p>
-            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/2254731?s=460&v=4"
-                alt="Diego Fernandes"
-              />
-              <div className="user-info">
-                <strong>Diego Fernandes</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>
-              CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de
-              desenvolvimento web e mobile.
-            </p>
-            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
